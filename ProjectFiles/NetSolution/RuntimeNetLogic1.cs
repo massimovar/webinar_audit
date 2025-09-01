67f22645f085f2f1c18eb7a7b296214b8f28dde1
MassimoVa <m.variolo@asem.it>
2025-09-01 17:26:07 +0200

    small UI changes

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,37 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.UI;
+using FTOptix.HMIProject;
+using FTOptix.NativeUI;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.EventLogger;
+using FTOptix.Recipe;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+
+    [ExportMethod]
+    public void MyMethod()
+    {
+        var input = Project.Current.GetVariable("Model/Audit Methods/VariableIN");
+        var output = Project.Current.GetVariable("Model/Audit Methods/VariableOUT");
+        output.Value = input.Value;
+    }
+}
